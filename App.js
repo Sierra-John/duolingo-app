7d250d26549d92501c1082037ab5fc1301599b17
John Sierra <sierrajp73@gmail.com>
2023-06-21 12:23:13 -0400

    Added props to component

--- App.js
+++ App.js
@@ -4,16 +4,20 @@ import { Text, View, Image } from "react-native";
 import styles from "./styles";
 
 import ImageOption from "./src/components/ImageOption/ImageOption";
+import question from "./assets/data/oneQuestionWithOption";
 
 function App() {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Which of these is the "glass"?</Text>
+      <Text style={styles.title}>{question.question}</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        {question.options.map((option) => (
+          <ImageOption
+            key={option.id}
+            image={option.image}
+            text={option.text}
+          />
+        ))}
       </View>
     </View>
   );
--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -3,19 +3,17 @@ import React from "react";
 
 import styles from "./styles";
 
-const ImageOption = () => {
-  return (
-    <View style={styles.optionContainer}>
-      <Image
-        source={{
-          uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png",
-        }}
-        resizeMode="contain"
-        style={styles.optionImage}
-      />
-      <Text style={styles.optionText}>Glass</Text>
-    </View>
-  );
-};
+const ImageOption = ({ image, text }) => (
+  <View style={styles.optionContainer}>
+    <Image
+      source={{
+        uri: image,
+      }}
+      resizeMode="contain"
+      style={styles.optionImage}
+    />
+    <Text style={styles.optionText}>{text}</Text>
+  </View>
+);
 
 export default ImageOption;
